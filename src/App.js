583fff2bf4a4eb007b23a692b50ec7000fcc6f9d
nasirdiu <nasir.joypurhat@gmail.com>
2022-10-28 22:42:47 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 class="text-red-500 text-2xl">
+    Hello world!
+  </h1>
+    </div>
+  );
+}
+
+export default App;
